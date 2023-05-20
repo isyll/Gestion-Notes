b3fcb2defce3345b4754d3ed7ad8b532dec6cc7d
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-20 12:34:47 +0000

    les méthodes SessionManager deviennent statiques

--- core/SessionManager.php
+++ core/SessionManager.php
@@ -4,27 +4,27 @@ namespace Core;
 
 class SessionManager
 {
-    public function __construct()
+    public static function start()
     {
         session_start();
     }
 
-    public function set(string $key, mixed $value): void
+    public static function set(string $key, mixed $value): void
     {
         $_SESSION[$key] = $value;
     }
 
-    public function get(string $key): mixed
+    public static function get(string $key): mixed
     {
         return $_SESSION[$key] ?? null;
     }
 
-    public function remove(string $key): void
+    public static function remove(string $key): void
     {
         unset($_SESSION[$key]);
     }
 
-    public function destroy()
+    public static function destroy()
     {
         session_destroy();
         $_SESSION = [];
