8e426ddbd3803f394f8b7a559cadeee6022883dd
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-21 19:24:51 +0000

    Page 404

---
+++ view/404.html.php
@@ -0,0 +1,6 @@
+<?php include 'parts/header.html.php'; ?>
+<main class="d-flex justify-content-center">
+    <div class="w-25 shadow-lg p-3 rounded">
+        <div class="fs-4 text-info text-center">La page que vous avez demandée n'existe pas</div>
+    </div>
+</main>
