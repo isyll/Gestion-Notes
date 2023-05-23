e0f0f03ca4313004e341eadb982b495cfa6912c4
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-23 18:21:36 +0000

    fonctionnalité de suppression d'une année scolaire

--- src/Controller/SchoolYearsController.php
+++ src/Controller/SchoolYearsController.php
@@ -30,18 +30,19 @@ class SchoolYearsController extends Controller
         if (SessionManager::get('school-create')) {
             $this->data['msg'] = SessionManager::get('school-create-msg');
 
-            SessionManager::remove('school-create');
-            SessionManager::remove('school-create-msg');
+            SessionManager::remove(['school-create', 'school-create-msg']);
+        } else if (SessionManager::get('school-remove')) {
+            $this->data['msg'] = SessionManager::get('school-remove-msg');
+
+            SessionManager::remove(['school-remove', 'school-remove-msg']);
         }
 
         echo $this->render('school-years', $this->data);
     }
 
-    public function createYear(string $year)
+    public function createYear()
     {
         SessionManager::set('school-create', true);
-
-        $this->data['current'] = 'school-years';
 
         $fv = new FormValidator(
             [
@@ -72,13 +73,56 @@ class SchoolYearsController extends Controller
                 Helpers::msg('Année déjà existante', 'danger')
             );
         } else {
-            SessionManager::set(
-                'school-create-msg',
-                Helpers::msg('Année créée avec succès', 'success')
-            );
             $this->model->saveYear([
                 'period' => $_POST['period']
             ]);
+
+            SessionManager::set(
+                'school-create-msg',
+                Helpers::msg('Année créée avec succès')
+            );
+        }
+
+        Helpers::redirectSite('/school-years');
+    }
+
+    public function removeYear()
+    {
+        SessionManager::set('school-remove', true);
+
+        $fv = new FormValidator(
+            [
+                [
+                    'value' => $_POST['yearId'],
+                    'type' => 'numeric',
+                    'name' => 'id',
+                    'required' => true
+                ]
+            ]
+        );
+
+        $fv->validate();
+
+        if (
+            count($this->data['errors'] = $fv->getErrors()) > 0
+        ) {
+            SessionManager::set(
+                'school-remove-msg',
+                Helpers::msg('Année non valide', 'danger')
+            );
+        } else if (!$this->model->yearExist((int) $_POST['yearId'])) {
+            SessionManager::set(
+                'school-remove-msg',
+                Helpers::msg("L'année demandée n'existe pas", 'danger')
+            );
+        } else {
+            $period = $this->model->getYearById((int) $_POST['yearId']);
+            $this->model->deleteYear((int) $_POST['yearId']);
+
+            SessionManager::set(
+                'school-remove-msg',
+                Helpers::msg("Année {$period['periode']} supprimée avec succès")
+            );
         }
 
         Helpers::redirectSite('/school-years');
--- view/school-years.html.php
+++ view/school-years.html.php
@@ -18,50 +18,66 @@
                         <div class="col-6">
                             <form class="d-flex" method="post" action="<?= $urls['create-year'] ?>">
                                 <label for="period" class="form-label me2">Libellé</label>
-                                <input type="text" maxlength="9" class="form-control ms-2" name="period" id="period" required/>
+                                <input type="text" maxlength="9" class="form-control ms-2" name="period" id="period"
+                                    required />
                                 <input type="submit" class="btn btn-secondary ms-2 text-white" value="Ajouter" />
                             </form>
                         </div>
                         <div class="col-3"></div>
                     </div>
                     <div class="row">
-                        <?php foreach ($schoolYears as $y): ?>
-                            <div class="card col-4">
-                                <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                                    <h5 role="btn" class="card-title text-center">
-                                        <?= $y['periode'] ?>
-                                    </h5>
-                                    <a href="/school-years/<?= $y['periode'] ?>" class="card-link">
-                                        <button type="button" class="btn btn-secondary text-white">Sélectionner</button>
-                                    </a>
+                        <div class="list-group">
+                            <?php foreach ($schoolYears as $y): ?>
+                                <div class="list-group-item list-group-item-action">
+                                    <div class="d-flex justify-content-between mb-2 w-100">
+                                        <h5>
+                                            <?= $y['periode'] ?>
+                                        </h5>
+                                        <div class="d-flex flex-column">
+                                            <a href="/school-years/<?= $y['periode'] ?>">Sélectionner</a>
+                                            <form action="" method="post" id="updateYear<?= $y['id'] ?>">
+                                                <input type="hidden" name="yearId" value="<?= $y['id'] ?>" />
+                                                <button type="submit" class="p-0 btn btn-link">Modifier</button>
+                                            </form>
+                                            <form action="/remove-year" method="post">
+                                                <input type="hidden" name="yearId" value="<?= $y['id'] ?>" />
+                                                <button type="submit"
+                                                    class="p-0 btn btn-link text-danger">Supprimer</button>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
+                            <?php endforeach ?>
+                        </div>
+                    </div>
+                    <div class="col-2"></div>
+                </div>
+                <!-- <div class="modal fade" id="confirmRemove" tabindex="-1" aria-labelledby="confirmRemoveLabel"
+                    aria-hidden="true" data-bs-backdrop="static">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="confirmRemoveLabel">Voulez-vous réellement supprimer
+                                    cette année</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
                             </div>
-                        <?php endforeach ?>
-                    </div>
-                </div>
-                <div class="col-2"></div>
-
-                <!-- <button data-bs-toggle="modal" data-bs-target="#modalAnneeScolaire"
-                    class="h-auto btn btn-light rounded col-2">Créer
-                    une année scolaire</button> -->
-            </div>
-            <div class="modal fade" id="modalAnneeScolaire" tabindex="-1" aria-labelledby="modalAnneeScolaireLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="modalAnneeScolaireLabel">Créer une année scolaire</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <?php include 'parts/forms/anneeform.html.php'; ?>
-                        </div>
-                        <div class="modal-footer">
+                            <div class="modal-body">
+                                <form method="post" action="/remove-year">
+                                    <input type="hidden" value="" />
+                                    <div class="mt-4 d-flex justify-content-center align-items-center">
+                                        <button type="button" class="me-2 btn btn-secondary text-white"
+                                            data-bs-dismiss="modal">Non</button>
+                                        <button type="submit" class="ms-2 text-white btn btn-primary">Oui</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </div> -->
 
+            </div>
         </div>
-    </div>
 </main>
