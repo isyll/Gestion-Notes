44bfc44e79a88ba5981ace1b758854b80621562f
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-24 12:44:50 +0000

    suppression méthode index()

--- src/Controller/ClassesController.php
+++ src/Controller/ClassesController.php
@@ -6,7 +6,6 @@ use App\Model\ClassesModel;
 use App\Model\NiveauxModel;
 use App\Model\SchoolYearsModel;
 use Core\Controller;
-use Core\Database;
 use Core\FormValidator;
 use Core\Helpers;
 
@@ -22,52 +21,6 @@ class ClassesController extends Controller
         $this->model = new ClassesModel($this->db);
         $this->nm    = new NiveauxModel($this->db);
         $this->sym   = new SchoolYearsModel($this->db);
-    }
-
-    public function index(string $id)
-    {
-        $this->data['current']   = 'niveaux';
-        $r                       = $this->nm->getNiveauById((int) $id);
-        $this->data['requested'] = [
-            'id' => $r[0]['id'],
-            'libelle' => $r[0]['libelle'],
-        ];
-        $this->data['exists']    = count($this->data['requested']) > 0;
-
-        if ($this->request['method'] === 'POST') {
-            $fv = new FormValidator([
-                [
-                    'required' => true,
-                    'name' => 'libellé',
-                    'value' => $_POST['libelleClasse'] ?? '',
-                ]
-            ]);
-
-            $fv->validate();
-
-            if (count($this->data['errors'] = $fv->getErrors()) > 0) {
-                $this->data['msg'] = Helpers::msg('Formulaire invalide', 'danger');
-            } else {
-                $this->data['classes'] = $this->nm->getClasses((int) $_POST['niveauId']);
-                if ($this->model->classeExist((int) $_POST['niveauId'], $_POST['libelleClasse'])) {
-                    $this->data['msg'] = Helpers::msg('Ce niveau possède déjà une classe nommée ' . $_POST['libelleClasse'], 'danger');
-                } else if ($this->nm->niveauExist((int) $_POST['niveauId'])) {
-                    $this->data['msg'] = Helpers::msg('Le niveau est incorrect', 'danger');
-                } else {
-                    $this->model->saveClasse($_POST);
-                    $this->data['msg']     = Helpers::msg('Classe créée avec succès');
-                    $this->data['classes'] = $this->nm->getClasses((int) $_POST['niveauId']);
-                }
-            }
-        } else {
-            if ($this->data['exists']) {
-                $this->data['classes'] = $this->nm->getClasses((int) $id);
-            } else {
-                $this->data['msg'] = Helpers::msg("Ce niveau n'existe pas", 'danger');
-            }
-        }
-
-        echo $this->render('classes', $this->data);
     }
 
     public function getClasses($period = NULL, $niveauSlug = NULL)
