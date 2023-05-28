7ff3cf80fec904fb813fcd09fa1bc3d7461b36da
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-28 13:02:31 +0000

    minification de la page 404

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
 
     public function page404()
     {
-        echo $this->render('404');
+        echo $this->render(file: '404', layout: false, minify: true);
     }
 
     public function test()
