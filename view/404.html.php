e15c98c475c0a0d5f02b319c8f67f4d44e76ccdf
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-28 13:03:09 +0000

    suppression inclusion dans la vue 404

--- view/404.html.php
+++ view/404.html.php
@@ -1,5 +1,36 @@
-<main class="d-flex justify-content-center w-100">
-    <div class="w-25 shadow-lg p-3 rounded text-center">
-        <div class="fs-4 text-danger text-center">La page que vous avez demandée n'existe pas</div>
-    </div>
-</main>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Page non trouvée
+    </title>
+    <link rel="shortcut icon" href="/img/logo-site.png">
+    <link rel="stylesheet" href="/css/bootstrap.css" />
+    <link rel="stylesheet" href="/css/bootstrap-icons.css" />
+    <link rel="stylesheet" href="/css/prism-okaidia.css" />
+    <link rel="stylesheet" href="/css/custom.css" />
+    <style>
+        * {
+            font-size: 102.5%;
+        }
+    </style>
+</head>
+
+<body class="text-center">
+
+    <main class="d-flex justify-content-center w-100">
+        <div class="w-25 shadow-lg p-3 rounded text-center">
+            <div class="fs-4 text-danger text-center">La page que vous avez demandée n'existe pas</div>
+        </div>
+    </main>
+    <script src="/js/jquery.js"></script>
+    <script src="/js/bootstrap-bundle.js"></script>
+    <script src="/js/prism.js"></script>
+    <script src="/js/custom.js"></script>
+</body>
+
+</html>
