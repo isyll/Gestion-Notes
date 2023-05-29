30883dfd54e04999fb9a7baabf2fa62dd2f406a8
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-29 11:44:11 +0000

    validation des données et corrections

--- src/Controller/StudentsController.php
+++ src/Controller/StudentsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Core\Controller;
+use Core\Router;
 
 class StudentsController extends Controller
 {
@@ -15,29 +16,36 @@ class StudentsController extends Controller
         $niveauId = NULL,
         $classeId = NULL
     ) {
-        $this->data['niveauId'] = (int) $niveauId;
-        $this->data['classeId'] = (int) $classeId;
+        if ($niveauId && $classeId) {
+            $niveauId = (int) $niveauId;
+            $classeId = (int) $classeId;
 
-        if ($this->niveauxModel->niveauIdExists($this->data['niveauId'])) {
-            $classe = $this->classesModel->getClasseById($this->data['classeId']);
-
-            if ($classe) {
-                if ($this->niveauxModel->hasClasse($this->data['niveauId'], $classe['libelle'])) {
-                    $this->data['students'] = $this->classesModel->getStudents($this->data['classeId']);
-                } else {
-                    $this->data['msg'] = $this->error("Le niveau sélectionné ne possède pas cette classe");
-                }
-            } else {
-                $this->data['msg'] = $this->error("La classe sélectionnée n'existe pas");
-            }
-        }
+            if ($this->niveauxModel->hasClasseId($niveauId, $classeId)) {
+                $this->data['niveau']   = $this->niveauxModel->getNiveauById($niveauId);
+                $this->data['classe']   = $this->classesModel->getClasseById($classeId);
+                $this->data['students'] = $this->classesModel->getStudents($classeId);
+            } else
+                Router::pageNotFound();
+        } else
+            Router::pageNotFound();
 
         echo $this->render('students', $this->data);
     }
 
-    public function newStudent()
+    public function newStudent($niveauId, $classeId)
     {
-        $this->data['niveaux'] = $this->niveauxModel->getNiveaux();
+        if ($niveauId && $classeId) {
+            $niveauId = (int) $niveauId;
+            $classeId = (int) $classeId;
+
+            if ($this->niveauxModel->hasClasseId($niveauId, $classeId)) {
+                $this->data['niveau']   = $this->niveauxModel->getNiveauById($niveauId);
+                $this->data['classe']   = $this->classesModel->getClasseById($classeId);
+                $this->data['students'] = $this->classesModel->getStudents($classeId);
+            } else
+                Router::pageNotFound();
+        } else
+            Router::pageNotFound();
 
         echo $this->render('new-student', $this->data);
     }
@@ -53,21 +61,23 @@ class StudentsController extends Controller
         if ($errors = $this->fv->getErrors()) {
             $this->session->set('msg', $this->error('Formulaire invalide'));
             $this->session->set('form-errors', $errors);
-            dd($errors);
         } else {
-            if ($this->studentsModel->getStudentByEmail($_POST['email'])) {
-                $this->session->set('form-errors', $this->error('Un autre élève possède le même email'));
-            } elseif ($this->studentsModel->getStudentByPhone($_POST['phone'])) {
-                $this->session->set('form-errors', $this->error('Un autre élève possède le même numéro de téléphone'));
+            if ($_POST['email'] && $this->studentsModel->getStudentByEmail($_POST['email'])) {
+                $this->session->set('msg', $this->error('Un autre élève possède le même email'));
+            } elseif ($_POST['phone'] && $this->studentsModel->getStudentByPhone($_POST['phone'])) {
+                $this->session->set('msg', $this->error('Un autre élève possède le même numéro de téléphone'));
             } elseif (!$this->niveauxModel->niveauIdExists((int) $_POST['niveauId'])) {
-                $this->session->set('form-errors', $this->error('La niveau sélectionné n\'existe pas'));
+                $this->session->set('msg', $this->error('La niveau sélectionné n\'existe pas'));
             } elseif (!$this->classesModel->classeIdExists((int) $_POST['classeId'])) {
-                $this->session->set('form-errors', $this->error('La classe sélectionnée n\'existe pas'));
+                $this->session->set('msg', $this->error('La classe sélectionnée n\'existe pas'));
             } elseif (!$this->classesModel->classeNiveauMatch((int) $_POST['classeId'], (int) $_POST['niveauId'])) {
-                $this->session->set('form-errors', $this->error('Le niveau ne correspond pas à la classe sélectionnée'));
+                $this->session->set('msg', $this->error('Le niveau ne correspond pas à la classe sélectionnée'));
             } else {
+                $_POST['annee_scolaire_id'] = $this->schoolYearsModel->getYearByLibelle($this->data['currentYear'])['id'];
                 $this->studentsModel->saveStudent($_POST);
                 $this->session->set('msg', $this->success('Elève créé avec succès'));
+
+                $this->redirect($this->data['urls']['list-students'] . "{$_POST['niveauId']}/{$_POST['classeId']}", false);
             }
         }
 
