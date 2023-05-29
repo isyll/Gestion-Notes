7cc4a5c4fbb0e0fd198f01e336f1758fc23f5c1d
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-29 11:52:08 +0000

    ajout required dans les champs de connexion

--- view/parts/forms/loginform.html.php
+++ view/parts/forms/loginform.html.php
@@ -2,14 +2,14 @@
     <input type="hidden" name="current-url" value="<?= $urls['current-url'] ?>" />
     <div class="mb-3 form-floating">
         <label for="username" class="form-label">Nom d'utilisateur</label>
-        <input type="text" class="form-control" id="username" name="username">
+        <input type="text" class="form-control" id="username" name="username" required>
         <div class="text-danger">
             <?= $errors['username'] ?? '' ?>
         </div>
     </div>
     <div class="mb-3 form-floating">
         <label for="password" class="form-label">Mot de passe</label>
-        <input type="password" class="form-control" id="password" name="password">
+        <input type="password" class="form-control" id="password" name="password" required>
         <div class="text-danger">
             <?= $errors['password'] ?? '' ?>
         </div>
