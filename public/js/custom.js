bc210e67969bf3591ba35293ea1cef7f6ce470bc
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-29 11:41:10 +0000

    confirmation de suppression de niveaux et classes

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,31 +1,9 @@
-$(function () {
-    const data = JSON.parse(document.getElementById("datas").textContent);
+$(".delete-btn").click(function (e) {
+    e.preventDefault();
+    const form = $(this).closest("form");
 
-    $("#niveauId").change(function (e) {
-        let val = $("#niveauId").val();
-        const getClasses = data["get-classe-by-niveau"];
-
-        if (val !== "") {
-            $.ajax({
-                method: "GET",
-                url: baseURL + getClasses + val,
-                dataType: "json",
-            })
-                .done(function (response) {
-                    let classes = response["datas"];
-                    let html = "";
-
-                    classes.forEach((element) => {
-                        html += `<option value="${element["id"]}">${element["libelle"]}</option>`;
-                    });
-
-                    $("#classeId").html(html);
-                })
-                .fail(function (error) {
-                    $("#classeId").html("");
-                });
-        } else {
-            $("#classeId").html("");
-        }
+    $("#modal-btn-yes").click(function () {
+        form.submit();
     });
 });
+
