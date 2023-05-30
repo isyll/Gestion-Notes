838a2ac23b8376f9d3a85cadbae8de9e3715e19b
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-30 15:52:45 +0000

    affichage liste des années scolaires

--- view/years.html.php
+++ view/years.html.php
@@ -9,7 +9,41 @@
             </div>
         <?php endif ?>
     </div>
-    <div class="row">
-        
+    <div class="row m-auto pt-2 col-10 col-lg-6">
+        <table class="table table-hover mt-2 table-striped">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody class="text-start">
+                <?php if (isset($years)): ?>
+                    <?php foreach ($years as $y): ?>
+                        <tr>
+                            <td>
+                                <?= $y['libelle'] ?>
+                            </td>
+                            <?php if ($y['libelle'] === $currentYear): ?>
+                                <td class="bg-success text-white">
+                                    Actif
+                                </td>
+                            <?php else: ?>
+                                <td class="bg-danger text-white">
+                                    Inactif
+                                </td>
+                            <?php endif ?>
+                            <td>
+                                <div class="form-check form-switch">
+                                    <input class="changeYearStateCheckbox form-check-input" type="checkbox" role="switch"
+                                        id="state" <?= $y['libelle'] === $currentYear ? 'checked' : '' ?> />
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                <?php endif ?>
+            </tbody>
+        </table>
     </div>
 </div>
