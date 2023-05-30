c9bcd47c383dc33dd3215ba11a08e0eb38504784
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-30 14:44:03 +0000

    suppression fichiers inutiles

--- public/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(".delete-btn").click(function (e) {
-    e.preventDefault();
-    const form = $(this).closest("form");
-
-    $("#modal-btn-yes").click(function () {
-        form.submit();
-    });
-});
-
