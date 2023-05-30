5defd2ef6f610d41cbc810de0b6324948cfd5c59
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-30 15:26:46 +0000

    création years.html.php

---
+++ view/years.html.php
@@ -0,0 +1,15 @@
+<header>
+    <?php include 'parts/navbar.html.php' ?>
+</header>
+<div class="container">
+    <div class="row">
+        <?php if (isset($msg)): ?>
+            <div class="text-<?= $msg['type'] ?>">
+                <?= $msg['value'] ?>
+            </div>
+        <?php endif ?>
+    </div>
+    <div class="row">
+        
+    </div>
+</div>
