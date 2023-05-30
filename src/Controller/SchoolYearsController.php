6320b2078792e63d488f29745703ec2ce233b808
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-30 15:21:17 +0000

    création SchoolYearsController

---
+++ src/Controller/SchoolYearsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Core\Controller;
+
+class SchoolYearsController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
