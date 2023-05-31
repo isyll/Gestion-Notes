8232e399640c2128c06abfcea340849a8bfdf997
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-31 21:08:23 +0000

    ajout fonction createGroup

--- src/Controller/SubjectsController.php
+++ src/Controller/SubjectsController.php
@@ -17,4 +17,24 @@ class SubjectsController extends Controller
 
         echo $this->render('subjects', $this->data, NULL, false, ['subjects']);
     }
+
+    public function createGroup()
+    {
+        $this->loadValidationRules('create-subject-group', $_POST);
+        $this->fv->validate();
+
+        if ($errors = $this->fv->getErrors()) {
+            $this->session->set('msg', $this->error('Formulaire invalide'));
+            $this->session->set('form-errors', $errors);
+        } elseif ($this->subjectsModel->groupNameExists($_POST['groupName'])) {
+            $this->session->set('msg', $this->error('Ce nom appartient déjà à un autre groupe'));
+        } else {
+            if ($this->subjectsModel->saveGroup($_POST))
+                $this->session->set('msg', $this->success('Groupe créé avec succès'));
+            else
+                $this->session->set('msg', $this->error("Une erreur inconnue s'est produite lors de l'insertion"));
+        }
+
+        $this->redirect($_POST['current-url'] ?? '', false);
+    }
 }
