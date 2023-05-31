838e36c2aa0549d646672904e6495045a1249e59
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-31 12:29:57 +0000

    ajout des id nécessaires au transfert

--- view/parts/forms/classeeditform.html.php
+++ view/parts/forms/classeeditform.html.php
@@ -1,7 +1,7 @@
 <form action="<?= $urls['edit-classe'] ?>" method="post" class="d-flex" id="editClasseForm">
     <input type="hidden" name="current-url" value="<?= $currentURL ?>" />
     <input type="hidden" name="niveauId" value="<?= $niveau['id'] ?>" />
-    <input type="hidden" name="classeId" value="" />
+    <input type="hidden" name="classeId" id="classeId" value="" />
     <input type="text" name="newClasseLibelle" id="newClasseLibelle" class="form-control align-self-center"
         placeholder="Nouveau nom" required />
     <button type="submit" class="btn btn-secondary align-self-center">Valider</button>
