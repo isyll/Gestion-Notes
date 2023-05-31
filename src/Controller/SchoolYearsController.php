ba07a0657fabcc2ff27e637902ba81af14d147d3
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-31 12:16:15 +0000

    fonction createYear, deleteYear et editYear

--- src/Controller/SchoolYearsController.php
+++ src/Controller/SchoolYearsController.php
@@ -17,4 +17,94 @@ class SchoolYearsController extends Controller
 
         echo $this->render('years', $this->data);
     }
+
+    public function createYear()
+    {
+        $this->loadValidationRules('create-year', $_POST);
+        $this->fv->validate();
+
+        if ($errors = $this->fv->getErrors()) {
+            $this->session->set('msg', $this->error('Formulaire invalide'));
+            $this->session->set('form-errors', $errors);
+        } else {
+            $years = explode('-', $_POST['libelle']);
+
+            if (
+                count($years) !== 2 ||
+                !is_numeric($years[0]) ||
+                !is_numeric($years[1]) ||
+                $years[1] - $years[0] !== 1
+            ) {
+                $this->session->set('msg', $this->error('Formulaire invalide'));
+                $this->session->set('form-errors', ['libelle' => 'Le libellé ne répond pas aux critères']);
+            } elseif ($this->schoolYearsModel->yearLibelleExists($_POST['libelle'])) {
+                $this->session->set('msg', $this->error('Cette année existe déjà'));
+            } else {
+                if ($this->schoolYearsModel->saveYear($_POST))
+                    $this->session->set('msg', $this->success("L'année a bien été enregistrée"));
+                else
+                    $this->session->set('msg', $this->error("Une erreur inconnue s'est produite"));
+            }
+        }
+
+        $this->redirect($_POST['current-url'] ?? '', false);
+    }
+
+    public function edit()
+    {
+        $this->loadValidationRules('edit-year', $_POST);
+        $this->fv->validate();
+
+        if ($errors = $this->fv->getErrors()) {
+            $this->session->set('msg', $this->error('Formulaire invalide'));
+            $this->session->set('form-errors', $errors);
+        } else {
+            $years = explode('-', $_POST['libelle']);
+
+            if (
+                count($years) !== 2 ||
+                !is_numeric($years[0]) ||
+                !is_numeric($years[1]) ||
+                $years[1] - $years[0] !== 1
+            ) {
+                $this->session->set('msg', $this->error('Formulaire invalide'));
+                $this->session->set('form-errors', ['libelle' => 'Le libellé ne répond pas aux critères']);
+            } else {
+                $this->schoolYearsModel->deleteYearById($_POST['yearId']);
+
+                if ($this->schoolYearsModel->getYearByLibelle($_POST['libelle'])) {
+                    $this->session->set('msg', $this->error('Cette année existe déjà'));
+                } else {
+                    if ($this->schoolYearsModel->editYear($_POST['yearId'], $_POST))
+                        $this->session->set('msg', $this->success("L'année a bien été enregistrée"));
+                    else
+                        $this->session->set('msg', $this->error("Une erreur inconnue s'est produite"));
+                }
+
+                $this->schoolYearsModel->restoreYearById($_POST['yearId']);
+            }
+        }
+
+        $this->redirect($_POST['current-url'] ?? '', false);
+    }
+
+    public function deleteYear()
+    {
+        $this->loadValidationRules('delete-year', $_POST);
+        $this->fv->validate();
+
+        if ($errors = $this->fv->getErrors()) {
+            $this->session->set('msg', $this->error('Formulaire invalide'));
+            $this->session->set('form-errors', $errors);
+        } elseif (!$this->schoolYearsModel->yearIdExists($_POST['yearId'])) {
+            $this->session->set('msg', $this->error("Cette année n'existe pas"));
+        } else {
+            if ($this->schoolYearsModel->deleteYearById($_POST['yearId']))
+                $this->session->set('msg', $this->success("L'année a bien été supprimée"));
+            else
+                $this->session->set('msg', $this->error("Une erreur inconnue est survenue"));
+        }
+
+        $this->redirect($_POST['current-url'] ?? '', false);
+    }
 }
