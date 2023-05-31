d9f9bb782b941bd019b36eab704aac237e862bbe
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-31 21:50:39 +0000

    recupération des groupes de disciplines

--- src/Controller/SubjectsController.php
+++ src/Controller/SubjectsController.php
@@ -14,6 +14,7 @@ class SubjectsController extends Controller
     public function page()
     {
         $this->data['niveaux'] = $this->niveauxModel->getNiveaux();
+        $this->data['groups']  = $this->subjectsModel->getGroups();
 
         echo $this->render('subjects', $this->data, NULL, false, ['subjects']);
     }
