68caeb0c0ed1d34e5f7bed3d7e4f6be098bd1f43
Ibrahima Sylla <syllaisa6@gmail.com>
2023-05-31 21:10:07 +0000

    améliorations de l'affichage

--- view/parts/forms/loginform.html.php
+++ view/parts/forms/loginform.html.php
@@ -1,15 +1,15 @@
 <form action="<?= $urls['login'] ?>" method="post">
     <input type="hidden" name="current-url" value="<?= $urls['current-url'] ?>" />
     <div class="mb-3 form-floating">
-        <label for="username" class="form-label">Nom d'utilisateur</label>
         <input type="text" class="form-control" id="username" name="username" required>
+        <label for="username">Nom d'utilisateur</label>
         <div class="text-danger">
             <?= $errors['username'] ?? '' ?>
         </div>
     </div>
     <div class="mb-3 form-floating">
-        <label for="password" class="form-label">Mot de passe</label>
         <input type="password" class="form-control" id="password" name="password" required>
+        <label for="password">Mot de passe</label>
         <div class="text-danger">
             <?= $errors['password'] ?? '' ?>
         </div>
