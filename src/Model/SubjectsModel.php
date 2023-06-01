353da7c670205e7f14738a5c1ed6a41d55670890
Ibrahima Sylla <syllaisa6@gmail.com>
2023-06-01 12:52:51 +0000

    ajout de fonctions pour gérer les groupes

--- src/Model/SubjectsModel.php
+++ src/Model/SubjectsModel.php
@@ -16,9 +16,18 @@ class SubjectsModel
     public function getGroups()
     {
         return $this->db->pexec(
-            'SELECT * FROM groupes_disciplines',
+            'SELECT * FROM groupes_disciplines WHERE supprime = 0',
             [],
             'fetchAll'
+        );
+    }
+
+    public function getGroupByName(string $name)
+    {
+        return $this->db->pexec(
+            'SELECT * FROM groupes_disciplines WHERE nom = ? AND supprime = 0',
+            [$name],
+            'fetch'
         );
     }
 
@@ -31,11 +40,44 @@ class SubjectsModel
         ) ? true : false;
     }
 
+    public function groupIdExists(int $id): bool
+    {
+        return $this->db->pexec(
+            'SELECT 1 FROM groupes_disciplines WHERE id = ?',
+            [$id],
+            'fetch'
+        ) ? true : false;
+    }
+
     public function saveGroup(array $data)
     {
         return $this->db->pexec(
             'INSERT INTO groupes_disciplines(nom) VALUES(?)',
             [$data['groupName']],
         ) ? true : false;
+    }
+
+    public function editGroup(int $id, array $data)
+    {
+        return $this->db->pexec(
+            'UPDATE groupes_disciplines SET nom = ? WHERE id = ?',
+            [$data['groupName'], $id],
+        ) ? true : false;
+    }
+
+    public function deleteGroupById(int $id)
+    {
+        return $this->db->pexec(
+            'UPDATE groupes_disciplines SET supprime = 1 WHERE id = ?',
+            [$id],
+        );
+    }
+
+    public function restoreGroupById(int $id)
+    {
+        return $this->db->pexec(
+            'UPDATE groupes_disciplines SET supprime = 0 WHERE id = ?',
+            [$id],
+        );
     }
 }
