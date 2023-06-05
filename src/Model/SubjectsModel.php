14c3026ab5edb7888f8a29e26c55096745f70b88
Ibrahima Sylla <syllaisa6@gmail.com>
2023-06-05 07:27:33 +0000

    ajout des fonctions de gestion des disciplines

--- src/Model/SubjectsModel.php
+++ src/Model/SubjectsModel.php
@@ -3,6 +3,7 @@
 namespace App\Model;
 
 use Core\Database;
+use Core\Helpers;
 
 class SubjectsModel
 {
@@ -24,8 +25,11 @@ class SubjectsModel
 
     public function subjectNameExists(string $name): bool
     {
+        if ($name === '')
+            return false;
+
         return $this->db->pexec(
-            'SELECT 1 FROM disciplines WHERE nom = ?',
+            'SELECT 1 FROM disciplines WHERE nom LIKE ?',
             [$name],
             'fetch'
         ) ? true : false;
@@ -33,6 +37,9 @@ class SubjectsModel
 
     public function subjectCodeExists(string $code): bool
     {
+        if ($code === '')
+            return false;
+
         return $this->db->pexec(
             'SELECT 1 FROM disciplines WHERE code = ?',
             [$code],
@@ -45,6 +52,15 @@ class SubjectsModel
         return $this->db->pexec(
             'SELECT * FROM groupes_disciplines WHERE nom = ? AND supprime = 0',
             [$name],
+            'fetch'
+        );
+    }
+
+    public function getGroupById(int $id)
+    {
+        return $this->db->pexec(
+            'SELECT * FROM groupes_disciplines WHERE id = ? AND supprime = 0',
+            [$id],
             'fetch'
         );
     }
@@ -79,6 +95,53 @@ class SubjectsModel
         );
     }
 
+    public function isClasseHasSubject(int $classeId, string $sbjName): bool
+    {
+        return $this->db->pexec(
+            'SELECT d.* FROM disciplines AS d
+            JOIN classes_disciplines AS cd ON cd.id_discipline = d.id
+            JOIN classes ON cd.id_classe = classes.id
+            AND classes.id = ?
+            WHERE d.nom = ?',
+            [$classeId, $sbjName],
+            'fetch'
+        ) ? true : false;
+    }
+
+    public function getSubjectByCode(string $code)
+    {
+        return $this->db->pexec(
+            'SELECT * FROM disciplines WHERE code = ?',
+            [$code],
+            'fetch'
+        );
+    }
+
+    public function getSubjectByName(string $name)
+    {
+        return $this->db->pexec(
+            'SELECT * FROM disciplines WHERE nom = ?',
+            [$name],
+            'fetch'
+        );
+    }
+
+    public function addSubjectToClasse(int $classeId, int $sbjId)
+    {
+        return $this->db->pexec(
+            'INSERT INTO classes_disciplines(id_classe, id_discipline) VALUES(?,?)',
+            [$classeId, $sbjId],
+        );
+    }
+
+    public function delSubjectFromClasse(int $classeId, int $sbjId)
+    {
+        return $this->db->pexec(
+            'DELETE FROM classes_disciplines WHERE id_classe = ? AND id_discipline = ?',
+            [$classeId, $sbjId],
+        );
+    }
+
     public function saveGroup(array $data)
     {
         return $this->db->pexec(
@@ -109,5 +172,44 @@ class SubjectsModel
             'UPDATE groupes_disciplines SET supprime = 0 WHERE id = ?',
             [$id],
         );
+    }
+
+    public function saveSubject(array $data)
+    {
+        return $this->db->pexec(
+            'INSERT INTO disciplines(nom, code, id_groupe) VALUES(?,?,?)',
+            [
+                $data['subjectName'],
+                $this->genSubjectCode($data['subjectName']),
+                $data['groupId']
+            ]
+        );
+    }
+
+    private function genSubjectCode(string $name): string
+    {
+        $name = trim($name);
+
+        if (count(explode(' ', $name)) === 1) {
+            $length = 3;
+
+            do {
+                $code = substr($name, 0, $length++);
+            } while ($this->subjectCodeExists($code));
+        } else {
+            $code = [];
+            preg_match_all('/\b[a-zA-Z]/', $name, $code);
+            $code = implode('', $code[0]);
+
+            $offset = 1;
+            $suffix = "";
+            while ($this->subjectCodeExists($code . $suffix)) {
+                $suffix .= substr($name, $offset++, 1);
+            }
+
+            $code .= $suffix;
+        }
+
+        return strtoupper($code);
     }
 }
